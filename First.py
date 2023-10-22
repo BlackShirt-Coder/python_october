0c823565903dae637ada22bd17d85691cd62e72d
BlackShirt-Coder <haruta685@gmail.com>
2023-10-22 16:55:20 +0630

    test

--- First.py
+++ First.py
@@ -1,6 +1,6 @@
 a=10
 b=20
-#print("a value is {} and b value is {}".format(a,b))
-#print("hello I'm {2} and I'm from {0} and I go to {1}".format("lewe","taungoo","mgmg"));
+print("a value is {} and b value is {}".format(a,b))
+print("hello I'm {2} and I'm from {0} and I go to {1}".format("lewe","taungoo","mgmg"));
 #key => value
 print("{bro} and {sis} are {fri} and they go to {goto}".format(bro="mgmg",sis="susu",goto="school",fri="friends"))
